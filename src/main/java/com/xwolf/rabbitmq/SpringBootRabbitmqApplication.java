e7071478f58b1b52b59cff03c5a84c502b56255b
xwolf <2324808561@qq.com>
2018-11-30 16:47:30 +0800

    init

---
+++ src/main/java/com/xwolf/rabbitmq/SpringBootRabbitmqApplication.java
@@ -0,0 +1,15 @@
+package com.xwolf.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xwolf
+ */
+@SpringBootApplication
+public class SpringBootRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitmqApplication.class, args);
+    }
+}
